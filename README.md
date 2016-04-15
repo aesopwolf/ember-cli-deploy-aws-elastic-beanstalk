c88630303ae135dfa4474c4aa6f241ccf84edad0
Aesop Wolf <aesopwolf@gmail.com>
2016-04-15 11:54:48 -0700

    init commit

--- README.md
+++ README.md
@@ -1,26 +1,42 @@
-# Ember-cli-deploy-elastic-beanstalk
+# ember-cli-deploy-elastic-beanstalk
 
-This README outlines the details of collaborating on this Ember addon.
+> This addon uploads a zipped file of your project to AWS Elastic Beanstalk
 
 ## Installation
+```
+$ ember install ember-cli-deploy-elastic-beanstalk
+```
 
-* `git clone` this repository
-* `npm install`
-* `bower install`
+## Quick Start
 
-## Running
+1. Optional: install [ember-cli-deploy-zip](https://github.com/aesopwolf/ember-cli-deploy-zip) to generate an `archive.zip` file automatically
+1. Use the following boilerplate to update `config/deploy.js`
+```js
+if (deployTarget === 'development') {
+  ENV.build.environment = 'development';
+  ENV.eb = {
+    // required:
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    REGION: 'us-west-1',
+    BUCKET: 'dev-example-com',
+    APPLICATION_NAME: 'example-app',
+    ENVIRONMENT_NAME: 'dev',
 
-* `ember server`
-* Visit your app at http://localhost:4200.
+    // optional. the plugin automatically defaults to these:
+    // essentially renames the source file to `ember-deploy-1460673054180.zip`
+    SOURCE_FILE: 'archive.zip',
+    TARGET_PREFIX: 'ember-deploy-',
+    TARGET_POSTFIX: Date.now(),
+    TARGET_EXTENSION: '.zip'
+  };
+}
+```
+1. Define `process.env.AWS_ACCESS_KEY_ID` inside a `.env` file in the root of your project. [Read more](http://ember-cli-deploy.github.io/ember-cli-deploy/docs/v0.6.x/dotenv-support/)
 
-## Running Tests
+## Usage
+```
+$ ember deploy <environment> ["optional description"]
+```
 
-* `npm test` (Runs `ember try:testall` to test your addon against multiple Ember versions)
-* `ember test`
-* `ember test --server`
-
-## Building
-
-* `ember build`
-
-For more information on using ember-cli, visit [http://ember-cli.com/](http://ember-cli.com/).
+For example" `ember deploy staging "deploying from my dev machine"`
--- index.js
+++ index.js
@@ -1,6 +1,128 @@
 /* jshint node: true */
 'use strict';
+var AWS = require('aws-sdk');
+var BasePlugin = require('ember-cli-deploy-plugin');
+var fs = require('fs');
+var ProgressBar = require('progress');
 
 module.exports = {
-  name: 'ember-cli-deploy-elastic-beanstalk'
+  name: 'ember-cli-deploy-elastic-beanstalk',
+  createDeployPlugin: function(options) {
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name,
+      configure: function(options) {
+        var self = this;
+
+        var required = [
+          'AWS_ACCESS_KEY_ID',
+          'AWS_SECRET_ACCESS_KEY',
+          'REGION',
+          'BUCKET',
+          'APPLICATION_NAME',
+          'ENVIRONMENT_NAME'
+        ];
+
+        var err;
+        required.forEach(function(value) {
+          if(!options.config.eb[value]) {
+            err = true;
+            self.log('Please add ' + value + ' to your ENV.eb object in config/deploy.js', { color: 'red' });
+          }
+        });
+
+        if(err) {
+          throw "- Deployment aborted: one or more of the required properties is missing";
+        }
+
+        AWS.config.update({
+          accessKeyId: options.config.eb.AWS_ACCESS_KEY_ID,
+          secretAccessKey: options.config.eb.AWS_SECRET_ACCESS_KEY,
+          region: options.config.eb.REGION,
+        });
+
+        return {
+          ebOptions: {
+            SOURCE_FILE: options.config.eb.SOURCE_FILE || 'archive.zip',
+            TARGET_PREFIX: options.config.eb.TARGET_PREFIX || 'ember-deploy-',
+            TARGET_POSTFIX: options.config.eb.TARGET_POSTFIX || Date.now(),
+            TARGET_EXTENSION: options.config.eb.TARGET_EXTENSION || '.zip'
+          }
+        };
+      },
+
+      upload: function(context) {
+        var bar = new ProgressBar('uploading [:bar] :percent :etas', {
+          complete: '=',
+          incomplete: ' ',
+          width: 20,
+          total: 1
+        });
+
+        return new Promise(function(resolve, reject) {
+          var body = fs.createReadStream(context.ebOptions.SOURCE_FILE);
+          var key = context.ebOptions.TARGET_PREFIX + context.ebOptions.TARGET_POSTFIX + context.ebOptions.TARGET_EXTENSION;
+          var s3obj = new AWS.S3({params: {Bucket: context.config.eb.BUCKET, Key: key}});
+          var previousChunk = 0;
+          var progress = 0;
+          s3obj.upload({Body: body})
+            .on('httpUploadProgress', function(e) {
+                progress = (e.loaded/e.total) - previousChunk;
+                previousChunk = (e.loaded/e.total);
+                bar.tick(progress);
+            })
+            .send(function(err, data) {
+              console.log('S3 console: https://console.aws.amazon.com/s3/home?region=' + context.config.eb.REGION + '#&bucket=' + context.config.eb.BUCKET);
+              console.log('S3 link: https://s3-' + context.config.eb.REGION +'.amazonaws.com/' + context.config.eb.BUCKET + '/' + key + '\n');
+              if(err) {
+                reject({error: err});
+              }
+              else {
+                resolve({upload: data});
+              }
+            });
+        });
+      },
+
+      didUpload: function(context) {
+        var elasticbeanstalk = new AWS.ElasticBeanstalk();
+
+        var params = {
+          ApplicationName: context.config.eb.APPLICATION_NAME,
+          AutoCreateApplication: true,
+          Description: process.argv[4] || '',
+          Process: true,
+          SourceBundle: {
+            S3Bucket: context.config.eb.BUCKET,
+            S3Key: context.upload.Key,
+          },
+          VersionLabel: context.upload.Key
+         };
+
+         return new Promise(function(resolve, reject) {
+           elasticbeanstalk.createApplicationVersion(params, function(err) {
+             if (err) {
+               reject(err);
+             }
+             else {
+               var params = {
+                 EnvironmentName: context.config.eb.ENVIRONMENT_NAME,
+                 VersionLabel: context.upload.Key
+                };
+                elasticbeanstalk.updateEnvironment(params, function(err, data) {
+                  if (err) {
+                    reject(err);
+                  }
+                  else {
+                    console.log('https://' + context.config.eb.REGION + '.console.aws.amazon.com/elasticbeanstalk/home?region=' + context.config.eb.REGION + '#/environment/dashboard?applicationName=' + data.ApplicationName + '&environmentId=' + data.EnvironmentId);
+                    resolve({didUpload: data});
+                  }
+                });
+             }
+           });
+         });
+      }
+    });
+
+    return new DeployPlugin();
+  }
 };
